7382bcf1062393969ace2237848b3def1fa3504f
weicheng <1648186705@qq.com>
2023-05-04 21:04:42 +0800

    perf:update

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <p>Power By <a href="https://github.com/BaoYi-9527/PHPTools">WeiCheng's Blog</a></p>
     <p class="record-info">
         <span><i class="fa fa-copyright"></i> 生活奇妙物语</span>
-        <span><a href="https://www.miit.gov.cn/" target="_blank">鄂ICP备2020016979号</a></span>
+        <span><a href="https://beian.miit.gov.cn/" target="_blank">鄂ICP备2020016979号</a></span>
     </p>
     <!--
     <p>
